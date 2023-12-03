2ae577152812aeef2a48997feb833f0c7018ad79
Ashraf-2022 <151772596+Ashraf-2022@users.noreply.github.com>
2023-12-04 00:37:49 +0200

    Update login_test.sql

--- login_test.sql
+++ login_test.sql
@@ -42,13 +42,6 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `md5_pass`, `date`, `gender`) VALUES
-(1, 'Ashraf Mohamed', 'adm193556@gmail.com', 'ashraf13552100', 'ea282aaa0dcaf89a172f838485be6d1a', '4-5-2005', 'male'),
-(2, 'Osama Salah', 'Osama1234@gmaol.com', '13552100@#123', '6ad5be9a35c0c9aa9d152d7533243d1c', '19-1-2002', 'male'),
-(3, 'Ahmed_Osama', 'Ahmed_2020@gamil.com', '01015611078@#', '$2y$10$i16/IYd/2.dyIGFBguvAmOLz5zvxlSmV6YJrHMDXpzi8US1pmISse', '15-2-1952', 'male'),
-(4, 'said hamdy', 'sayed22@gmail.com', '12345678900', '$2y$10$p0itdwu/lYGgTXAIlWtda.BZ/2UUdsk7JOHYCJ0.i6pSP5qG0XoaW', '13-1-1991', 'male'),
-(5, 'Samira1212', 'Samira1212@hotmail.com', '13552100@#$%^&amp;*()', '$2y$10$nC9bo94cg9s0T7S.SWLDMOJp6XUTCqKs9WdnOBm8mTMIGDlO/ioAe', '18-6-2001', 'female'),
-(6, 'abd elsalam', 'abd_elsalam@gmail.com', '12345678900987654321', '$2y$10$ujQk7DXnhqNTOmbSj.gfHet1DyMeciYWUTxK8bCZspRBSG9sunHLO', '18-11-1951', 'male'),
-(7, 'Ramy Saied', 'Ramy_Saied@gmail.com', 'Ramy Saied', '$2y$10$tS5K6yuU7HQLeyXJHDlubev8W/hAoKBSBP1jQzqaJ0pmWCTt3sS3u', '19-7-1994', 'male');
 
 --
 -- Indexes for dumped tables
